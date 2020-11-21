eae8792e8acdd3a24a6cd7971f10d88996889f08
xray <xyaguilar77@gmail.com>
2020-11-21 18:43:32 -0500

    Updates

---
+++ precache-manifest.da966526bf4d28fa9d4a0da3302f26ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741056fd4fc0e725ae0a379ce400ce4f",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "e8274f5784119586c88d",
+    "url": "/employee-directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "d4729884788454186a73",
+    "url": "/employee-directory/static/css/main.820063cc.chunk.css"
+  },
+  {
+    "revision": "e8274f5784119586c88d",
+    "url": "/employee-directory/static/js/2.ddf63b60.chunk.js"
+  },
+  {
+    "revision": "c53f65871ab74006a0c4e0b92b224267",
+    "url": "/employee-directory/static/js/2.ddf63b60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4729884788454186a73",
+    "url": "/employee-directory/static/js/main.a12a2c5b.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
