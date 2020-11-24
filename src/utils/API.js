3bd3e54fcffcfeadad5b01ff84619faf54731907
Xyra Arroyo <xyaguilar77@gmail.com>
2020-11-24 16:50:43 -0500

    Update API.js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default {
-  // Export an object containing methods we'll use for accessing the Dog.Ceo API
   retrievePeople() {
     return axios.get('https://randomuser.me/api/?results=50')
   },
